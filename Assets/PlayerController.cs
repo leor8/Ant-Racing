a1fa33e9dcb9ded6d27fa0ea69e775388cbe2fe9
leor8 <leo442183205@gmail.com>
2018-10-05 19:08:05 -0700

    now powerups pick up is partially working and updating graphics

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+
+    public KeyCode left, right, jump, powerup1, powerup2, powerup3, powerup4, powerup5;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public LayerMask thisLayer;
+    private Inventory inventory;
+
+    public  bool isGrounded;
+
+    private bool printed = false;
+
+    private Rigidbody2D rb;
+
+    // powerup variables
+    private bool speeded = false;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        inventory = GetComponent<Inventory>();
+    }
+
+    void Update () {
+
+
+        // Checking if the player is on ground otherwise the player should not be able to jump mid-air
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        // Getting key inputs to move the player
+        if(Input.GetKey(left)) {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        } else if (Input.GetKey(right)) {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        } else {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        // For jumping
+        if(Input.GetKeyDown(jump) && isGrounded) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if(Input.GetKey(powerup1)){
+            // Speed up
+            if(inventory.isFull[0]) {
+                Destroy(inventory.slots[0]);
+                inventory.isFull[0] = false;
+                if(!speeded) {
+                    speed += 10;
+                    speeded = true;
+                }
+
+            }
+        }
+
+        if(Input.GetKey(powerup2)) {
+            // Jump Higher
+            if(inventory.isFull[1]) {
+                Destroy(inventory.slots[1]);
+                inventory.isFull[1] = false;
+                if(!speeded) {
+                    jumpForce += 10;
+                    speeded = true;
+                }
+            }
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if(coll.gameObject.CompareTag("Collectables")){
+            Destroy(coll.gameObject);
+            // int curr_powerup = Random.Range(0, powerup_pool.Length);
+            // inventory.Add(powerup_pool[curr_powerup]);
+        }
+
+    }
+
+    void using_powerup() {
+
+    }
+}
---
+++ Assets/pickup.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+  private Inventory inventory;
+  private Inventory inventory2;
+  public GameObject itemButton;
+  private GameObject newItem;
+
+	// Use this for initialization
+	void Start () {
+		inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    inventory2 = GameObject.FindGameObjectWithTag("Player2").GetComponent<Inventory>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+  void OnTriggerEnter2D(Collider2D coll) {
+    if(coll.CompareTag("Player")) {
+      for(int i = 0; i < inventory.slots.Length; i++) {
+        if(!inventory.isFull[i]){
+          newItem = Instantiate(itemButton, inventory.slots[i].transform);
+          newItem.transform.localScale += new Vector3(1.5F, 1F, 0);
+          newItem.transform.position = new Vector2(newItem.transform.position.x + 0.7f, newItem.transform.position.y + 0.9f);
+          inventory.isFull[i] = true;
+          Destroy(gameObject);
+          break;
+        }
+      }
+    } else if (coll.CompareTag("Player2")){
+      for(int i = 0; i < inventory2.slots.Length; i++) {
+        if(!inventory2.isFull[i]){
+          newItem = Instantiate(itemButton, inventory2.slots[i].transform);
+          newItem.transform.localScale += new Vector3(1.5F, 1F, 0);
+          newItem.transform.position = new Vector2(newItem.transform.position.x + 0.7f, newItem.transform.position.y + 0.9f);
+          inventory2.isFull[i] = true;
+          Destroy(gameObject);
+          break;
+        }
+      }
+    }
+
+  }
+}
