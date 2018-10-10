d04e82bc5024c77e75e83a6657c5b94c5698430e
leor8 <leo442183205@gmail.com>
2018-10-10 16:25:54 -0700

    setting up scenes

---
+++ Assets/Scripts/load_scene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load_scene : MonoBehaviour {
+  public void SceneLoader(int sceneIndex) {
+    SceneManager.LoadScene(sceneIndex);
+  }
+}
