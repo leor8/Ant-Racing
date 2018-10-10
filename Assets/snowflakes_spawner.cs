956aaacb3cf6a27a7e7e2330594aea0dd641a13d
leor8 <leo442183205@gmail.com>
2018-10-09 20:19:09 -0700

    snowflakes spawning on processing

---
+++ Assets/snowflakes_spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snowflakes_spawner : MonoBehaviour {
+  public GameObject snowflake;
+  float randX;
+  float randY;
+  Vector2 spawn_location;
+  public float spawn_rate = 2f;
+  float nextSpawn = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+    if(Time.time > nextSpawn) {
+      nextSpawn = Time.time + spawn_rate;
+      randX = Random.Range(-27.1f, 23.7f);
+      randY = Random.Range(99.1f, 136.3f);
+      spawn_location = new Vector2(randX, randY);
+    }
+	}
+}
